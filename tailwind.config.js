54a640ca3a535be35432a77f9a885f4c3848667d
Shruti Balasa <thirusofficial@gmail.com>
2021-01-10 15:42:19 +0530

    Portfolio website ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
-  purge: [
-    './public/*.html'
-  ],
+  purge: {
+    content: ['./public/**/*.html'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: {
+        6: '6px'
+      },
+      colors: {
+        'cyan': colors.cyan,
+        'emerald': colors.emerald,
+      },
+    },
   },
   variants: {
     extend: {},
